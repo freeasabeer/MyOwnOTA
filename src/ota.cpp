8b5a2ab1dc113eef05ece8226743937f22ad3baa
Olivier Perron <olivier.perron12@gmail.com>
2024-12-20 21:39:42 +0100

    v1.1.0
    Use ARDUINO_EVENT_xxxxx instead of SYSTEM_EVENT_xxxxx in the Wifi event
    callback.
    Enhanced debug messages for Wifi events.

--- src/ota.cpp
+++ src/ota.cpp
@@ -7,7 +7,88 @@
   #include <ArduinoOTA.h>
   #include <SPIFFS.h>
 #endif
+#include <map>
 #include "ota.h"
+
+/*
+std::map<int, const char *> systemEventWifiCode {
+  {SYSTEM_EVENT_WIFI_READY,         "SYSTEM_EVENT_WIFI_READY < ESP32 WiFi ready"},
+  {SYSTEM_EVENT_SCAN_DONE,              "SYSTEM_EVENT_SCAN_DONE < ESP32 finish scanning AP"},
+  {SYSTEM_EVENT_STA_START,              "SYSTEM_EVENT_STA_START < ESP32 station start"},
+  {SYSTEM_EVENT_STA_STOP,               "SYSTEM_EVENT_STA_STOP < ESP32 station stop"},
+  {SYSTEM_EVENT_STA_CONNECTED,          "SYSTEM_EVENT_STA_CONNECTED < ESP32 station connected to AP"},
+  {SYSTEM_EVENT_STA_DISCONNECTED,       "SYSTEM_EVENT_STA_DISCONNECTED < ESP32 station disconnected from AP"},
+  {SYSTEM_EVENT_STA_AUTHMODE_CHANGE,    "SYSTEM_EVENT_STA_AUTHMODE_CHANGE < the auth mode of AP connected by ESP32 station changed"},
+  {SYSTEM_EVENT_STA_GOT_IP,             "SYSTEM_EVENT_STA_GOT_IP < ESP32 station got IP from connected AP"},
+  {SYSTEM_EVENT_STA_LOST_IP,            "SYSTEM_EVENT_STA_LOST_IP < ESP32 station lost IP and the IP is reset to 0"},
+  {SYSTEM_EVENT_STA_BSS_RSSI_LOW,       "SYSTEM_EVENT_STA_BSS_RSSI_LOW < ESP32 station connected BSS rssi goes below threshold"},
+  {SYSTEM_EVENT_STA_WPS_ER_SUCCESS,     "SYSTEM_EVENT_STA_WPS_ER_SUCCESS < ESP32 station wps succeeds in enrollee mode"},
+  {SYSTEM_EVENT_STA_WPS_ER_FAILED,      "SYSTEM_EVENT_STA_WPS_ER_FAILED < ESP32 station wps fails in enrollee mode"},
+  {SYSTEM_EVENT_STA_WPS_ER_TIMEOUT,     "SYSTEM_EVENT_STA_WPS_ER_TIMEOUT < ESP32 station wps timeout in enrollee mode"},
+  {SYSTEM_EVENT_STA_WPS_ER_PIN,         "SYSTEM_EVENT_STA_WPS_ER_PIN < ESP32 station wps pin code in enrollee mode"},
+  {SYSTEM_EVENT_STA_WPS_ER_PBC_OVERLAP, "SYSTEM_EVENT_STA_WPS_ER_PBC_OVERLAP < ESP32 station wps overlap in enrollee mode"},
+  {SYSTEM_EVENT_AP_START,               "SYSTEM_EVENT_AP_START < ESP32 soft-AP start"},
+  {SYSTEM_EVENT_AP_STOP,                "SYSTEM_EVENT_AP_STOP < ESP32 soft-AP stop"},
+  {SYSTEM_EVENT_AP_STACONNECTED,        "SYSTEM_EVENT_AP_STACONNECTED < a station connected to ESP32 soft-AP"},
+  {SYSTEM_EVENT_AP_STADISCONNECTED,     "SYSTEM_EVENT_AP_STADISCONNECTED < a station disconnected from ESP32 soft-AP"},
+  {SYSTEM_EVENT_AP_STAIPASSIGNED,       "SYSTEM_EVENT_AP_STAIPASSIGNED < ESP32 soft-AP assign an IP to a connected station"},
+  {SYSTEM_EVENT_AP_PROBEREQRECVED,      "SYSTEM_EVENT_AP_PROBEREQRECVED < Receive probe request packet in soft-AP interface"},
+  {SYSTEM_EVENT_ACTION_TX_STATUS,       "SYSTEM_EVENT_ACTION_TX_STATUS < Receive status of Action frame transmitted"},
+  {SYSTEM_EVENT_ROC_DONE,               "SYSTEM_EVENT_ROC_DONE < Indicates the completion of Remain-on-Channel operation status"},
+  {SYSTEM_EVENT_STA_BEACON_TIMEOUT,     "SYSTEM_EVENT_STA_BEACON_TIMEOUT < ESP32 station beacon timeout"},
+  {SYSTEM_EVENT_FTM_REPORT,             "SYSTEM_EVENT_FTM_REPORT < Receive report of FTM procedure"},
+  {SYSTEM_EVENT_GOT_IP6,                "SYSTEM_EVENT_GOT_IP6 < ESP32 station or ap or ethernet interface v6IP addr is preferred"},
+  {SYSTEM_EVENT_ETH_START,              "SYSTEM_EVENT_ETH_START < ESP32 ethernet start"},
+  {SYSTEM_EVENT_ETH_STOP,               "SYSTEM_EVENT_ETH_STOP < ESP32 ethernet stop"},
+  {SYSTEM_EVENT_ETH_CONNECTED,          "SYSTEM_EVENT_ETH_CONNECTED < ESP32 ethernet phy link up"},
+  {SYSTEM_EVENT_ETH_DISCONNECTED,       "SYSTEM_EVENT_ETH_DISCONNECTED < ESP32 ethernet phy link down"},
+  {SYSTEM_EVENT_ETH_GOT_IP,             "SYSTEM_EVENT_ETH_GOT_IP < ESP32 ethernet got IP from connected AP"},
+  {SYSTEM_EVENT_ETH_LOST_IP,            "SYSTEM_EVENT_ETH_LOST_IP < ESP32 ethernet lost IP and the IP is reset to 0"}
+};
+*/
+std::map<int, const char *> arduinoEventWifiCode {
+  {ARDUINO_EVENT_WIFI_READY,                "ARDUINO_EVENT_WIFI_READY"},
+  {ARDUINO_EVENT_WIFI_SCAN_DONE,            "ARDUINO_EVENT_WIFI_SCAN_DONE"},
+  {ARDUINO_EVENT_WIFI_STA_START,            "ARDUINO_EVENT_WIFI_STA_START"},
+  {ARDUINO_EVENT_WIFI_STA_STOP,             "ARDUINO_EVENT_WIFI_STA_STOP"},
+  {ARDUINO_EVENT_WIFI_STA_CONNECTED,        "ARDUINO_EVENT_WIFI_STA_CONNECTED"},
+  {ARDUINO_EVENT_WIFI_STA_DISCONNECTED,     "ARDUINO_EVENT_WIFI_STA_DISCONNECTED"},
+  {ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE,  "ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE"},
+  {ARDUINO_EVENT_WIFI_STA_GOT_IP,           "ARDUINO_EVENT_WIFI_STA_GOT_IP"},
+  {ARDUINO_EVENT_WIFI_STA_GOT_IP6,          "ARDUINO_EVENT_WIFI_STA_GOT_IP6"},
+  {ARDUINO_EVENT_WIFI_STA_LOST_IP,          "ARDUINO_EVENT_WIFI_STA_LOST_IP"},
+  {ARDUINO_EVENT_WIFI_AP_START,             "ARDUINO_EVENT_WIFI_AP_START"},
+  {ARDUINO_EVENT_WIFI_AP_STOP,              "ARDUINO_EVENT_WIFI_AP_STOP"},
+  {ARDUINO_EVENT_WIFI_AP_STACONNECTED,      "ARDUINO_EVENT_WIFI_AP_STACONNECTE"},
+  {ARDUINO_EVENT_WIFI_AP_STADISCONNECTED,   "ARDUINO_EVENT_WIFI_AP_STADISCONNECTED"},
+  {ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED,     "ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED"},
+  {ARDUINO_EVENT_WIFI_AP_PROBEREQRECVED,    "ARDUINO_EVENT_WIFI_AP_PROBEREQRECVED"},
+  {ARDUINO_EVENT_WIFI_AP_GOT_IP6,           "ARDUINO_EVENT_WIFI_AP_GOT_IP6"},
+  {ARDUINO_EVENT_WIFI_FTM_REPORT,           "ARDUINO_EVENT_WIFI_FTM_REPORT"},
+  {ARDUINO_EVENT_ETH_START,                 "ARDUINO_EVENT_ETH_START"},
+  {ARDUINO_EVENT_ETH_STOP,                  "ARDUINO_EVENT_ETH_STOP"},
+  {ARDUINO_EVENT_ETH_CONNECTED,             "ARDUINO_EVENT_ETH_CONNECTED"},
+  {ARDUINO_EVENT_ETH_DISCONNECTED,          "ARDUINO_EVENT_ETH_DISCONNECTED"},
+  {ARDUINO_EVENT_ETH_GOT_IP,                "ARDUINO_EVENT_ETH_GOT_IP"},
+  {ARDUINO_EVENT_ETH_GOT_IP6,               "ARDUINO_EVENT_ETH_GOT_IP6"},
+  {ARDUINO_EVENT_WPS_ER_SUCCESS,            "ARDUINO_EVENT_WPS_ER_SUCCESS"},
+  {ARDUINO_EVENT_WPS_ER_FAILED,             "ARDUINO_EVENT_WPS_ER_FAILED"},
+  {ARDUINO_EVENT_WPS_ER_TIMEOUT,            "ARDUINO_EVENT_WPS_ER_TIMEOUT"},
+  {ARDUINO_EVENT_WPS_ER_PIN,                "ARDUINO_EVENT_WPS_ER_PIN"},
+  {ARDUINO_EVENT_WPS_ER_PBC_OVERLAP,        "ARDUINO_EVENT_WPS_ER_PBC_OVERLAP"},
+  {ARDUINO_EVENT_SC_SCAN_DONE,              "ARDUINO_EVENT_SC_SCAN_DONE"},
+  {ARDUINO_EVENT_SC_FOUND_CHANNEL,          "ARDUINO_EVENT_SC_FOUND_CHANNEL"},
+  {ARDUINO_EVENT_SC_GOT_SSID_PSWD,          "ARDUINO_EVENT_SC_GOT_SSID_PSWD"},
+  {ARDUINO_EVENT_SC_SEND_ACK_DONE,          "ARDUINO_EVENT_SC_SEND_ACK_DONE"},
+  {ARDUINO_EVENT_PROV_INIT,                 "ARDUINO_EVENT_PROV_INIT"},
+  {ARDUINO_EVENT_PROV_DEINIT,               "ARDUINO_EVENT_PROV_DEINIT"},
+  {ARDUINO_EVENT_PROV_START,                "ARDUINO_EVENT_PROV_START"},
+  {ARDUINO_EVENT_PROV_END,                  "ARDUINO_EVENT_PROV_END"},
+  {ARDUINO_EVENT_PROV_CRED_RECV,            "ARDUINO_EVENT_PROV_CRED_RECV"},
+  {ARDUINO_EVENT_PROV_CRED_FAIL,            "ARDUINO_EVENT_PROV_CRED_FAIL"},
+  {ARDUINO_EVENT_PROV_CRED_SUCCESS,         "ARDUINO_EVENT_PROV_CRED_SUCCESS"}
+};
+
 
 static void StaticTimerCallbackFunction(TimerHandle_t xTimer) {
   MyOwnOTA* me = static_cast<MyOwnOTA*>(pvTimerGetTimerID(xTimer));
@@ -29,9 +110,11 @@ MyOwnOTA::MyOwnOTA() {
 ///////////////////////////////////////////////////////////////////////
 
 void MyOwnOTA::OTA_WiFi_AP_Event(WiFiEvent_t event) {
-  debug("[WiFi-event] event: %d\n", event);
+  //debug("[WiFi-event] event: %d\n", event);
+  //debug("[System  WiFi-event]: %d > %s\n", event, systemEventWifiCode[event]);
+  debug("[Arduino WiFi-event]: %d > %s\n", event, arduinoEventWifiCode[event]);
   switch(event) {
-  case SYSTEM_EVENT_AP_START:
+  case ARDUINO_EVENT_WIFI_AP_START:
       debug("WiFi AP started\n");
       debug("IP address: %s\n", WiFi.softAPIP().toString().c_str());
       debug("Network ID: %s\n", WiFi.softAPNetworkID().toString().c_str());
@@ -39,18 +122,18 @@ void MyOwnOTA::OTA_WiFi_AP_Event(WiFiEvent_t event) {
       if (_otaconfig->cb !=NULL)
         (*_otaconfig->cb)(WiFi.softAPIP().toString().c_str());
       break;
-  case SYSTEM_EVENT_AP_STOP:
+  case ARDUINO_EVENT_WIFI_AP_STOP:
       debug("WiFi AP stopped\n");
       break;
-  case SYSTEM_EVENT_AP_STACONNECTED:
+  case ARDUINO_EVENT_WIFI_AP_STACONNECTED:
       debug("a station connected to ESP32 soft-AP\n");
       debug("%d client(s) connected.\n", WiFi.softAPgetStationNum());
       break;
-  case SYSTEM_EVENT_AP_STADISCONNECTED:
+  case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED:
       debug("a station disconnected from ESP32 soft-AP\n");
       debug("%d client(s) connected.\n", WiFi.softAPgetStationNum());
       break;
-  case SYSTEM_EVENT_AP_STAIPASSIGNED:
+  case ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED:
       debug("ESP32 soft-AP assign an IP to a connected station\n");
       break;
   default:
@@ -84,15 +167,17 @@ void MyOwnOTA::OTA_connectToWifi(TimerHandle_t xTimer) {
 
 
 void MyOwnOTA::OTA_WiFi_Client_Event(WiFiEvent_t event) {
-  debug("[WiFi-event] event: %d\n", event);
+  //debug("[WiFi-event] event: %d\n", event);
+  //debug("[System  WiFi-event]: %d > %s\n", event, systemEventWifiCode[event]);
+  debug("[Arduino WiFi-event]: %d > %s\n", event, arduinoEventWifiCode[event]);
   switch(event) {
-    case SYSTEM_EVENT_STA_GOT_IP:
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
       debug("WiFi connected\n");
       debug("IP address: %s\n", WiFi.localIP().toString().c_str());
       if (_otaconfig->cb !=NULL)
         (*_otaconfig->cb)(WiFi.localIP().toString().c_str());
       break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
+    case ARDUINO_EVENT_WIFI_STA_CONNECTED:
       debug("WiFi lost connection\n");
       xTimerStart(_OTAwifiReconnectTimer, 0);
       break;
